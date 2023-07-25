90462248b66cc31423b5432ddaeed99218b4f198
Raffaele <raffaele.gerosa@cern.ch>
2023-07-25 15:32:05 +0200

    first import for the whole production

---
+++ crab_miniaod_production/Era2022EE/QCD_HTbinned_BEnriched_Madgraph/digi_raw_hlt_step.py
@@ -0,0 +1,182 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: --python_filename EGM-Run3Summer22EEDRPremix-00004_1_cfg.py --eventcontent PREMIXRAW --customise Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM-RAW --fileout file:EGM-Run3Summer22EEDRPremix-00004_0.root --conditions 124X_mcRun3_2022_realistic_postEE_v1 --step DIGI,DATAMIX,L1,DIGI2RAW,HLT:2022v14 --procModifiers premix_stage2,siPixelQualityRawToDigi --geometry DB:Extended --filein file:EGM-Run3Summer22EEwmLHEGS-00001.root --datamix PreMix --era Run3 --no_exec --mc -n 10
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Eras.Era_Run3_cff import Run3
+from Configuration.ProcessModifiers.premix_stage2_cff import premix_stage2
+from Configuration.ProcessModifiers.siPixelQualityRawToDigi_cff import siPixelQualityRawToDigi
+
+from FWCore.ParameterSet.VarParsing import VarParsing
+options = VarParsing ('analysis')
+options.register('nThreads', 1, VarParsing.multiplicity.singleton,VarParsing.varType.int,"nThreads")
+options.register('inputName',  "genSim_step.root", VarParsing.multiplicity.singleton,VarParsing.varType.string,"inputName")
+options.register('outputName', "digiRaw_step.root", VarParsing.multiplicity.singleton,VarParsing.varType.string,"outputName")
+options.register('pileupName', "pileup.py", VarParsing.multiplicity.singleton,VarParsing.varType.string,"pileupName")
+options.parseArguments()
+
+process = cms.Process('HLT',Run3,premix_stage2,siPixelQualityRawToDigi)
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.DigiDM_cff')
+process.load('Configuration.StandardSequences.DataMixerPreMix_cff')
+process.load('Configuration.StandardSequences.SimL1EmulatorDM_cff')
+process.load('Configuration.StandardSequences.DigiToRawDM_cff')
+process.load('HLTrigger.Configuration.HLT_2022v14_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+
+# Input source
+process.source = cms.Source("PoolSource",
+    dropDescendantsOfDroppedBranches = cms.untracked.bool(False),
+    fileNames = cms.untracked.vstring('file:EGM-Run3Summer22EEwmLHEGS-00001.root'),
+    inputCommands = cms.untracked.vstring(
+        'keep *',
+        'drop *_genParticles_*_*',
+        'drop *_genParticlesForJets_*_*',
+        'drop *_kt4GenJets_*_*',
+        'drop *_kt6GenJets_*_*',
+        'drop *_iterativeCone5GenJets_*_*',
+        'drop *_ak4GenJets_*_*',
+        'drop *_ak7GenJets_*_*',
+        'drop *_ak8GenJets_*_*',
+        'drop *_ak4GenJetsNoNu_*_*',
+        'drop *_ak8GenJetsNoNu_*_*',
+        'drop *_genCandidatesForMET_*_*',
+        'drop *_genParticlesForMETAllVisible_*_*',
+        'drop *_genMetCalo_*_*',
+        'drop *_genMetCaloAndNonPrompt_*_*',
+        'drop *_genMetTrue_*_*',
+        'drop *_genMetIC5GenJs_*_*'
+    ),
+    secondaryFileNames = cms.untracked.vstring()
+)
+
+if not options.inputFiles:
+    process.source.fileNames = cms.untracked.vstring('file:'+options.inputName)
+else:
+    process.source.fileNames = cms.untracked.vstring(options.inputFiles)
+
+    
+process.options = cms.untracked.PSet(
+    FailPath = cms.untracked.vstring(),
+    IgnoreCompletely = cms.untracked.vstring(),
+    Rethrow = cms.untracked.vstring(),
+    SkipEvent = cms.untracked.vstring(),
+    accelerators = cms.untracked.vstring('*'),
+    allowUnscheduled = cms.obsolete.untracked.bool,
+    canDeleteEarly = cms.untracked.vstring(),
+    deleteNonConsumedUnscheduledModules = cms.untracked.bool(True),
+    dumpOptions = cms.untracked.bool(False),
+    emptyRunLumiMode = cms.obsolete.untracked.string,
+    eventSetup = cms.untracked.PSet(
+        forceNumberOfConcurrentIOVs = cms.untracked.PSet(
+            allowAnyLabel_=cms.required.untracked.uint32
+        ),
+        numberOfConcurrentIOVs = cms.untracked.uint32(0)
+    ),
+    fileMode = cms.untracked.string('FULLMERGE'),
+    forceEventSetupCacheClearOnNewRun = cms.untracked.bool(False),
+    makeTriggerResults = cms.obsolete.untracked.bool,
+    numberOfConcurrentLuminosityBlocks = cms.untracked.uint32(0),
+    numberOfConcurrentRuns = cms.untracked.uint32(1),
+    numberOfStreams = cms.untracked.uint32(options.nThreads),
+    numberOfThreads = cms.untracked.uint32(options.nThreads),
+    printDependencies = cms.untracked.bool(False),
+    sizeOfStackForThreadsInKB = cms.optional.untracked.uint32,
+    throwIfIllegalParameter = cms.untracked.bool(True),
+    wantSummary = cms.untracked.bool(False)
+)
+
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('digi_raw_step.py nevts:-1'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+
+# Output definition
+
+process.PREMIXRAWoutput = cms.OutputModule("PoolOutputModule",
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('GEN-SIM-RAW'),
+        filterName = cms.untracked.string('')
+    ),
+    fileName = cms.untracked.string('file:'+options.outputName),
+    outputCommands = process.PREMIXRAWEventContent.outputCommands,
+    splitLevel = cms.untracked.int32(0)
+)
+
+# Additional output definition
+
+# Other statements
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '124X_mcRun3_2022_realistic_postEE_v1', '')
+
+# Pileup mixing
+import os,random
+random.seed = os.urandom(1000)
+puListFull = [];
+with open(options.pileupName,'r') as pileup:
+    for element in pileup:
+        puListFull.append(element);
+random.shuffle(puListFull)
+process.mixData.input.fileNames = cms.untracked.vstring(puListFull)
+
+
+# Path and EndPath definitions
+process.digitisation_step = cms.Path(process.pdigi)
+process.datamixing_step = cms.Path(process.pdatamix)
+process.L1simulation_step = cms.Path(process.SimL1Emulator)
+process.digi2raw_step = cms.Path(process.DigiToRaw)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.PREMIXRAWoutput_step = cms.EndPath(process.PREMIXRAWoutput)
+
+# Schedule definition
+# process.schedule imported from cff in HLTrigger.Configuration
+process.schedule.insert(0, process.digitisation_step)
+process.schedule.insert(1, process.datamixing_step)
+process.schedule.insert(2, process.L1simulation_step)
+process.schedule.insert(3, process.digi2raw_step)
+process.schedule.extend([process.endjob_step,process.PREMIXRAWoutput_step])
+from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
+associatePatAlgosToolsTask(process)
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from Configuration.DataProcessing.Utils
+from Configuration.DataProcessing.Utils import addMonitoring 
+
+#call to customisation function addMonitoring imported from Configuration.DataProcessing.Utils
+process = addMonitoring(process)
+
+# Automatic addition of the customisation function from HLTrigger.Configuration.customizeHLTforMC
+from HLTrigger.Configuration.customizeHLTforMC import customizeHLTforMC 
+
+#call to customisation function customizeHLTforMC imported from HLTrigger.Configuration.customizeHLTforMC
+process = customizeHLTforMC(process)
+
+# End of customisation functions
+
+
+# Customisation from command line
+
+# Add early deletion of temporary data products to reduce peak memory need
+from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEarlyDelete
+process = customiseEarlyDelete(process)
+# End adding early deletion
---
+++ crab_miniaod_production/Era2022EE/QCD_HTbinned_BEnriched_Madgraph/scriptExe.sh
@@ -0,0 +1,27 @@
+#!/bin/bash                                                                                                                                                                                       
+set -e
+BASE=$PWD
+RELEASE_BASE=$CMSSW_BASE
+
+export SCRAM_ARCH=el8_amd64_gcc10
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+
+echo "setting up CMSSW environment"
+cd $RELEASE_BASE
+eval `scram runtime -sh`
+cd $BASE
+
+echo "cmsRun -e -j FrameworkJobReport.xml gen_sim_step.py jobNum="$1" nEvents="$2" "$3" outputName=genSimStep.root gridpack="$4
+cmsRun -e -j FrameworkJobReport.xml gen_sim_step.py jobNum=$1 nEvents=$2 $3 outputName=genSimStep.root gridpack=$4
+
+echo "cmsRun -e -j FrameworkJobReport.xml digi_raw_step.py "$3" inputFiles=genSimStep.root outputName=digirawStep.root"
+cmsRun -e -j FrameworkJobReport.xml digi_raw_step.py $3 inputFiles=genSimStep.root outputName=digirawStep.root
+rm genSimStep.root
+
+echo "cmsRun -e -j FrameworkJobReport.xml reco_hlt_step.py "$3" inputName=digirawStep.root outputName=recoHltStep.root"
+cmsRun -e -j FrameworkJobReport.xml reco_hlt_step.py $3 inputName=digirawStep.root outputName=recoHltStep.root
+rm digirawStep.root
+
+echo "cmsRun -e -j FrameworkJobReport.xml miniaod_step.py "$3" inputName=recoHltStep.root "$4
+cmsRun -e -j FrameworkJobReport.xml miniaod_step.py $3 inputName=recoHltStep.root $4
+rm recoHltStep.root
