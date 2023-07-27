8551f745dda4f4ef8d46349ef8a5690689ffbd97
Raffaele <raffaele.gerosa@cern.ch>
2023-07-27 09:39:22 +0200

    adding all files needed for production

--- crab_miniaod_production/Era2022EE/QCD_HTbinned_BEnriched_Madgraph/scriptExe.sh
+++ crab_miniaod_production/Era2022EE/QCD_HTbinned_BEnriched_Madgraph/scriptExe.sh
@@ -11,15 +11,15 @@ cd $RELEASE_BASE
 eval `scram runtime -sh`
 cd $BASE
 
-echo "cmsRun -e -j FrameworkJobReport.xml gen_sim_step.py jobNum="$1" nEvents="$2" "$3" outputName=genSimStep.root gridpack="$4
-cmsRun -e -j FrameworkJobReport.xml gen_sim_step.py jobNum=$1 nEvents=$2 $3 outputName=genSimStep.root gridpack=$4
+echo "cmsRun -e -j FrameworkJobReport.xml gen_sim_step.py jobNum="$1" "$2" "$3" outputName=genSimStep.root "$5
+cmsRun -e -j FrameworkJobReport.xml gen_sim_step.py jobNum=$1 $2 $3 outputName=genSimStep.root $5
 
-echo "cmsRun -e -j FrameworkJobReport.xml digi_raw_step.py "$3" inputFiles=genSimStep.root outputName=digirawStep.root"
-cmsRun -e -j FrameworkJobReport.xml digi_raw_step.py $3 inputFiles=genSimStep.root outputName=digirawStep.root
+echo "cmsRun -e -j FrameworkJobReport.xml digi_raw_hlt_step.py "$3" inputName=genSimStep.root outputName=digirawStep.root"
+cmsRun -e -j FrameworkJobReport.xml digi_raw_hlt_step.py $3 inputName=genSimStep.root outputName=digirawStep.root
 rm genSimStep.root
 
-echo "cmsRun -e -j FrameworkJobReport.xml reco_hlt_step.py "$3" inputName=digirawStep.root outputName=recoHltStep.root"
-cmsRun -e -j FrameworkJobReport.xml reco_hlt_step.py $3 inputName=digirawStep.root outputName=recoHltStep.root
+echo "cmsRun -e -j FrameworkJobReport.xml reco_step.py "$3" inputName=digirawStep.root outputName=recoHltStep.root"
+cmsRun -e -j FrameworkJobReport.xml reco_step.py $3 inputName=digirawStep.root outputName=recoHltStep.root
 rm digirawStep.root
 
 echo "cmsRun -e -j FrameworkJobReport.xml miniaod_step.py "$3" inputName=recoHltStep.root "$4
